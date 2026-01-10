35a6f5182125476ad3ef5544e9597113a6572452
Simon Bürger <simon.buerger@rwth-aachen.de>
2026-01-11 00:53:03 +0100

    better adhere to time limit

--- src/metis/negamax.cpp
+++ src/metis/negamax.cpp
@@ -13,9 +13,35 @@ NegamaxEngine::NegamaxEngine(util::Json const &j)
 	eval_ = std::make_shared<LinearEvaluator>(j.at("eval"));
 }
 
-int NegamaxEngine::search(Board const &board, int depth, int alpha, int beta,
-                          std::stop_token const &stoken)
+std::optional<int> NegamaxEngine::search(Board const &board, int depth,
+                                         int alpha, int beta)
 {
+	// note: 'depth' can be below zero inside quiescence search.
+	// note: illegal positions dont count into "node_count": Move-generation
+	// creating some illegal moves is an implementation detail. Future:
+	// cache-hits do count.
+	if (!board.legal())
+		return -32000;
+
+	node_count_++;
+
+	auto should_stop = [&]() -> bool {
+		if (node_count_ >= node_limit_)
+			return true;
+		if (node_count_ % 100 != 0) // rate-limit non-trivial checks
+			return false;
+		if (stoken_.stop_requested())
+			return true;
+		auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
+		                   Clock::now() - start_time_)
+		                   .count();
+		if (elapsed >= time_limit_)
+			return true;
+		return false;
+	};
+	if (should_stop())
+		return {};
+
 	if (board.checkmate())
 		return -31000;
 	if (board.draw())
@@ -38,58 +64,49 @@ int NegamaxEngine::search(Board const &board, int depth, int alpha, int beta,
 	generate_pseudolegal_moves(board, moves, depth <= 0);
 	for (auto move : moves)
 	{
-		auto score = search(board, move, depth - 1,
-		                    std::max(alpha, best_score + 1), beta, stoken);
+		auto score =
+		    search(board, move, depth, std::max(alpha, best_score + 1), beta);
 
-		if (score > beta)
+		if (!score)
+			return {};
+
+		if (*score > beta)
 			return score;
 
-		if (score > best_score)
-			best_score = score;
+		if (*score > best_score)
+			best_score = *score;
 	}
 	return best_score;
 }
 
-int NegamaxEngine::search(Board board, Move move, int depth, int alpha,
-                          int beta, std::stop_token const &stoken)
+std::optional<int> NegamaxEngine::search(Board board, Move move, int depth,
+                                         int alpha, int beta)
 {
 	board.make_move(move);
-	if (!board.legal())
-		return -32000;
-	auto score = -search(board, depth, -beta, -alpha, stoken);
+	auto score = search(board, depth - 1, -beta, -alpha);
+	if (!score)
+		return {};
+	*score = -*score;
 
 	// mate-scores are adjusted to favor shorter mates
-	if (score > 30000)
-		score -= 1;
-	if (score < -30000)
-		score += 1;
-	return score;
+	if (*score > 30000)
+		*score -= 1;
+	if (*score < -30000)
+		*score += 1;
+	return *score;
 }
 
 void NegamaxEngine::think(Board const &board, ProgressCallback progress,
                           std::stop_token stoken, int time_limit)
 {
-	using Clock = std::chrono::steady_clock;
-	typename Clock::time_point search_start = Clock::now();
-
-	int slack = 5;
-	int movetime = std::min(options_.time_limit, time_limit);
 	node_count_ = 0;
+	start_time_ = Clock::now();
+	node_limit_ = INT_MAX; // TODO
+	time_limit_ = time_limit;
+	stoken_ = stoken;
+	int depth_limit = INT_MAX;
 
-	auto should_stop = [&]() {
-		if (stoken.stop_requested())
-			return true;
-		if (movetime != INT_MAX)
-		{
-			auto elapsed =
-			    std::chrono::duration_cast<std::chrono::milliseconds>(
-			        Clock::now() - search_start);
-			if (elapsed.count() >= movetime)
-				return true;
-		}
-		return false;
-	};
-
+	// generate all legal moves
 	MoveList moves;
 	generate_pseudolegal_moves(board, moves);
 	erase_if(moves, [&](Move const &move) {
@@ -98,69 +115,51 @@ void NegamaxEngine::think(Board const &board, ProgressCallback progress,
 		return !new_board.legal();
 	});
 
+	// only one legal move: no need to search
 	assert(!moves.empty());
 	if (moves.size() == 1)
 	{
-		// only one legal move, no need to send any detailed info
 		progress({.best_move = moves[0]});
 		return;
 	}
 
 	int best_score = -32000;
 	Move best_move = moves[0];
-	int second_best_score = -32000;
-	Move second_best_move = moves[1];
 
-	for (int depth = 1; depth <= options_.depth_limit; ++depth)
+	for (int depth = 1; depth <= depth_limit; ++depth)
 	{
-		// search the best move from previous depth
-		best_score = search(board, best_move, depth - 1, -32000, 32000, stoken);
-		if (should_stop())
+		// full search the best move from previous depth
+		if (auto score = search(board, best_move, depth, -32000, 32000))
+			best_score = *score;
+		else
 			break;
-		// TODO: report new score/PV, even though it is the same move
-		second_best_score = -32000;
 
+		// search alternative moves with alpha-pruning
 		for (auto move : moves)
 		{
 			if (move == best_move)
 				continue;
 
-			auto score = search(board, move, depth - 1, second_best_score + 1,
-			                    32000, stoken);
-			if (score > best_score)
-			{
-				second_best_score = best_score;
-				second_best_move = best_move;
-				best_score = score;
-				best_move = move;
-
-				AnalysisResult result;
-				result.best_move = best_move;
-				result.score = best_score;
-				result.depth = depth;
-				result.nodes = node_count_;
-				progress(result);
-			}
-			else if (score > second_best_score)
-			{
-				second_best_score = score;
-				second_best_move = move;
-			}
-
-			if (should_stop())
+			auto score = search(board, move, depth, best_score + 1, 32000);
+			if (!score)
 				break;
+
+			if (*score > best_score)
+			{
+				best_score = *score;
+				best_move = move;
+			}
 		}
+
+		AnalysisResult result;
+		result.best_move = best_move;
+		result.score = best_score;
+		result.depth = depth;
+		result.nodes = node_count_;
+		progress(result);
 	}
 
-	auto chosen_move = best_move;
-	if (-20000 < best_score && best_score < 20000)
-		if (second_best_score > best_score - slack)
-			if (rng.bernoulli())
-				chosen_move = second_best_move;
-	progress({.best_move = chosen_move});
-
-	if (stoken.stop_requested())
-		return;
+	progress({.best_move = best_move});
 }
 
 } // namespace metis
--- src/metis/negamax.h
+++ src/metis/negamax.h
@@ -5,8 +5,18 @@
 #include "util/json.h"
 #include <cstdint>
 #include <memory>
+#include <optional>
 
 namespace metis {
+
+// On scores:
+//   * positive score means good for the side to move
+//   * scores use 'int' for convenience, but actually everything is
+//     scaled/clamped to fit into int16_t
+//   * scores from the evaluator are clamped by the engine to [-10'000, 10'000].
+//     Everything outside this range is for sure win/loss:
+//       * +-(31'000-k) = mate in k
+//       * -32'000 = illegal position
 
 // minimax with alpha-beta pruning
 class NegamaxEngine final : public Engine
@@ -14,11 +24,7 @@ class NegamaxEngine final : public Engine
   public:
 	struct Options
 	{
-		// These limits are per invocation of `think()`
-		int depth_limit = INT_MAX;
-		int node_limit = INT_MAX;
-		int time_limit = INT_MAX; // milliseconds
-
+		// enable/disable quiescence search
 		bool qsearch = true;
 
 		Options() = default;
@@ -32,14 +38,30 @@ class NegamaxEngine final : public Engine
 	Options options_;
 	std::shared_ptr<Evaluator> eval_;
 
-	// statistics
+	// these are reset at the start of each '.think()' call
 	int64_t node_count_ = 0;
+	using Clock = std::chrono::steady_clock;
+	Clock::time_point start_time_;
+	int64_t node_limit_ = INT64_MAX;
+	int time_limit_ = INT_MAX; // milliseconds
+	std::stop_token stoken_;
 
-	// recursive search function
-	int search(Board const &, int depth, int alpha, int beta,
-	           std::stop_token const &);
-	int search(Board board, Move move, int depth, int alpha, int beta,
-	           std::stop_token const &);
+	// recursive search function.
+	//   * If search was interrupted (time/node limit), returns std::nullopt.
+	//   * if depth==0, does quiescence search.
+	//   * if result is outside [alpha, beta], it is a lower/upper bound instead
+	//     of an exact score.
+	//   * future: due to caching, the reult might be somewhat unstable. In
+	//     particular if a higher-depth result from cache might always be used
+	//     in place of a lower-depth search.
+	std::optional<int> search(Board const &, int depth, int alpha, int beta);
+
+	// score 'board' assuming 'move' is played next.
+	//   * typically equal to -search(board after move, ...), but also adjusts
+	//     mate-scores.
+	//   * future: might be correct place to check for repetitions?
+	std::optional<int> search(Board board, Move move, int depth, int alpha,
+	                          int beta);
 
   public:
 	explicit NegamaxEngine(util::Json const &j);
